7e5e5c17146f65e859c7c0910fc37ac8caf6cb55
HoodaHashem <mahmoudhashem335@gmail.com>
2025-04-26 02:56:22 +0300

    Feat: Add logout and is-logged-in routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { checkSchema } from "express-validator";
 import { loginValidation, signupValidation } from "../validations/auth.js";
-import { login, signup } from "../controllers/auth.js";
+import { isLoggedIn, login, logout, signup } from "../controllers/auth.js";
 
 const authRouter = Router()
 
 authRouter.post("/signup", checkSchema(signupValidation), signup)
 authRouter.post("/login", checkSchema(loginValidation), login)
-// authRouter.post("/logout", logout)
+authRouter.get("/logout", logout)
+authRouter.get("/is-logged-in", isLoggedIn)
+
 // authRouter.post("/refresh-token", refreshToken)
 // authRouter.post("/forgot-password", forgotPassword)
 // authRouter.post("/reset-password", resetPassword)
