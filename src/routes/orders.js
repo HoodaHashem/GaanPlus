8e2ec71bec9e1712e6a65486f97016ea12de305a
HoodaHashem <mahmoudhashem335@gmail.com>
2025-04-26 02:58:52 +0300

    Feat: Basic structure of orders router

---
+++ src/routes/orders.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { checkSchema } from "express-validator";
+
+
+// import { createOrder, getOrders, getOrderById, updateOrder, deleteOrder } from "../controllers/orders.js";
+
+const ordersRouter = Router();
+// ordersRouter.get("/", getOrders);
+// ordersRouter.get("/:id", getOrderById);
+// ordersRouter.post("/", createOrder);
+// ordersRouter.put("/:id", updateOrder);
+// ordersRouter.delete("/:id", deleteOrder);
+
+
+export default ordersRouter;
